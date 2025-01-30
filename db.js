5d8e4440ee3361789f71a2437172f8bae92a4940
andrija14 <andrija.zecevic098@gmail.com>
2025-01-30 22:54:26 +0100

    backend31

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
 const pool = mysql.createPool({
-    connectionLimit: 10, // Maksimalni broj konekcija u pool-u
-    host: 'localhost',
-    user: 'root',
-    password: 'andrija2005',
-    database: 'meksiko',
+    host: 'bl0dfdtukgwdl9hjrwo5-mysql.services.clever-cloud.com',
+    user: 'u0mee5xgufkiciv6',
+    password: '6TwWjIT2LmaANtupiIdI',
+    database: 'bl0dfdtukgwdl9hjrwo5',
+    port: 3306,
 });
 
 module.exports = pool;
